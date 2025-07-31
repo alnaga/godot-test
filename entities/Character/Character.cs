a968ba6984195de3b5ddc1cf992650a024ee5b03
Alex Naggar <alexgrahamnaggar@hotmail.com>
2025-07-31 23:14:49 +0200

    more basic setup + add TODO

--- entities/Character/Character.cs
+++ entities/Character/Character.cs
@@ -1,41 +1,41 @@
 using Godot;
 
+// TODO: Make Hadrian Pac-Man
 public partial class Character : Node2D
 {
     private Vector2 Velocity = new(0, 0);
+    private bool IsDebug = true;
 
     public override void _Process(double delta)
     {
         HandleInput();
-
-
-        Position = CalculatePosition(Velocity, delta);
+        UpdatePosition(delta);
+        PrintDebug();
     }
 
     private void HandleInput()
     {
         if (Input.IsActionPressed("move_right"))
         {
-            Move(new Vector2(1, 0));
+            UpdateVelocity(new Vector2(1, 0));
         }
         if (Input.IsActionPressed("move_left"))
         {
-            Move(new Vector2(-1, 0));
+            UpdateVelocity(new Vector2(-1, 0));
         }
         if (Input.IsActionPressed("move_up"))
         {
-            Move(new Vector2(0, -1));
+            UpdateVelocity(new Vector2(0, -1));
         }
         if (Input.IsActionPressed("move_down"))
         {
-            Move(new Vector2(0, 1));
+            UpdateVelocity(new Vector2(0, 1));
         }
     }
 
     public override void _Draw()
     {
         base._Draw();
-        GD.Print("Character position: ", Position);
 
         // Move the "Body" Polygon2D to match the Character's Position
         var body = GetNodeOrNull<Polygon2D>("CharacterBody2D/Body");
@@ -45,28 +45,26 @@ public partial class Character : Node2D
         }
     }
 
-    public override void _Ready()
+    private void UpdatePosition(double delta)
     {
-        base._Ready();
-        GD.Print("Character ready");
+        Position += Velocity * (float)delta * 100;
     }
 
-    private Vector2 CalculatePosition(Vector2 direction, double delta)
-    {
-        var newPosition = Position + direction * (float)delta * 100;
-        return newPosition;
-    }
-
-    private Vector2 CalculateVelocity(Vector2 direction)
-    {
-        var newVelocity = Velocity + direction;
-        return newVelocity;
-    }
-
-    public void Move(Vector2 direction)
+    private void UpdateVelocity(Vector2 direction)
     {
         Velocity += direction;
         Velocity = Velocity.Normalized() * 1;
-        GD.Print("Velocity: ", Velocity);
+    }
+
+    private void PrintDebug()
+    {
+        if (!IsDebug)
+        {
+            return;
+        }
+
+        // GD.Print("Position: ", Position);
+        // GD.Print("Velocity: ", Velocity);
+        // GD.Print("IsDebug: ", IsDebug);
     }
 }
