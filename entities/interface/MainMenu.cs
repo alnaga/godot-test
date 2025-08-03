66370f76f5bb4eb44d61a0c2264cc4603d3ef805
Alex Naggar <alexgrahamnaggar@hotmail.com>
2025-08-03 20:33:49 +0200

    add UI to main menu

--- entities/interface/MainMenu.cs
+++ entities/interface/MainMenu.cs
@@ -2,25 +2,41 @@ using Godot;
 
 public partial class MainMenu : Control
 {
+    private Button playButton;
+    private Button quitButton;
+
     public override void _Ready()
     {
-        // Example: Connect a start button if you have one
-        // GetNode<Button>("StartButton").Pressed += OnStartButtonPressed;
+        playButton = GetNode<Button>("VBoxContainer/PlayButton");
+        quitButton = GetNode<Button>("VBoxContainer/QuitButton");
+
+        playButton.Pressed += OnStartButtonPressed;
+        quitButton.Pressed += OnQuitButtonPressed;
     }
 
     public override void _Input(InputEvent @event)
     {
         // If click on the screen, start game
-        if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
-        {
-            StartGame();
-        }
+        // if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.Pressed)
+        // {
+        //     StartGame();
+        // }
+
+        // if (@event is InputEventKey keyEvent && keyEvent.Keycode == Key.Escape)
+        // {
+        //     OnQuitButtonPressed();
+        // }
     }
 
     private void StartGame()
     {
         // Use utility method to change game state
         GameManager.SetGameState(this, GameState.Game);
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        GetTree().Quit();
     }
 
     private void OnStartButtonPressed()
